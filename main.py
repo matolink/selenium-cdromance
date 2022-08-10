639f16bce7b65a8b251079435d98b37524d7a023
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-08-10 12:43:23 -0400

    second commit, added region and console filter

--- main.py
+++ main.py
@@ -2,35 +2,77 @@ import os
 import time
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import Levenshtein as lev
+
 DIR = '/home/matito/Downloads/'
-REG = 'USA' #Europe
-driver = uc.Chrome()
-excel = 'P.n. 03' #aquí va el texto exportado del excel
+REG = 'Region USA' #Europe
+CON = 'GCN'
 lev2 = 99
-driver.get("https://cdromance.com/")
+excel = 'Killer 7'
+list1 = []
+list2 = []
+detlist = []
+regionlist = []
+a = []
+cn = []
+li = []
+
+driver = uc.Chrome()
 driver.implicitly_wait(30)
+driver.get("https://cdromance.com/")
 sb = driver.find_element(By.CLASS_NAME, "search-field")
 sb.click()
 sb.send_keys(excel) 
 sb.submit()
 gc = driver.find_elements(By.CLASS_NAME, 'game-container')
-li = driver.find_elements(By.CLASS_NAME, 'game-title')
-for items in li:
-    print(items.text)
-    lev1 = lev.distance(items.text,excel)
+
+for i in range(len(gc)):
+    detlist.append(gc[i].find_element(By.CLASS_NAME, 'details'))
+
+for i in range(len(detlist)):
+    regionlist.append(detlist[i].find_element(By.CLASS_NAME, 'region'))
+
+for i in range(len(regionlist)):
+    if regionlist[i].get_attribute("title") == REG:
+        list1.append(gc[i])
+        print('se agrega a la lista list1[filtro region]')
+
+for i in range(len(list1)):
+    a.append(list1[i].find_element(By.TAG_NAME, 'a'))
+
+for i in range(len(a)):
+    cn.append(a[i].find_element(By.CLASS_NAME, 'console'))
+
+for i in range(len(cn)):
+    if cn[i].text == CON:
+        list2.append(list1[i])
+        print('se agrega a la lista list2[filtro consola]')
+
+for i in range(len(list2)):
+    li.append(list2[i].find_element(By.CLASS_NAME, 'game-title'))
+
+for titles in li:
+    print(titles.text)
+    lev1 = lev.distance(titles.text,excel)
     if lev1 < lev2:
         lev2 = lev1
-        final_text = items.text
-for items in li:
-    if final_text == items.text:
-        game = items
+        final_text = titles.text
+        print(final_text)
+
+for i in range(len(li)):
+    if final_text == li[i].text:
+        game = list2[i]
+
 game.click()
+
+
+
 dl = driver.find_element(By.ID, 'dl-btn-0')
 name = dl.get_attribute("data-filename")
 dl.click()
 while True:
-        if os.path.exists(dir + name):
+        if os.path.exists(DIR + name):
             break
         else:
             print('else')
